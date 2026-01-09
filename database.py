4546b324343c415994ffa1c4f4d810e962a13398
Nekigy <156176199+nekigy@users.noreply.github.com>
2026-01-09 21:40:13 +0300

    Add files via upload

---
+++ database.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+def init_db():
+    conn = sqlite3.connect("shop.db")
+    cur = conn.cursor()
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS orders (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        telegram_id INTEGER,
+        uc INTEGER,
+        price INTEGER,
+        status TEXT
+    )
+    """)
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS admins (
+        telegram_id INTEGER PRIMARY KEY
+    )
+    """)
+
+    conn.commit()
+    conn.close()
